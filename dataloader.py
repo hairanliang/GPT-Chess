561cc600ac25d5b1a2960a26dedfe5cf068d0ff3
Hairan <hairanliang@gmail.com>
2024-07-13 01:16:35 +0800

    implemented custom tokenizer and trains altho poorly

--- dataloader.py
+++ dataloader.py
@@ -4,66 +4,140 @@ import chess.pgn
 import matplotlib.pyplot as plt
 import numpy as np
 from util import produce_pairs
-
+import torch
 
 # Design choice: have the dataloader call the utility function which then handles the input target pairs here
 
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+# tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
-tokenizer.pad_token = tokenizer.eos_token
+# tokenizer.pad_token = tokenizer.eos_token
 
+
+# What will be fed in is a list of moves, for which all that has to be done is convert them to the token through a simple for loop
 class ChessDataset(Dataset):
-    def __init__(self, games, tokenizer, max_length):
+    def __init__(self, games, token_dict, max_length):
+        # print("initialized")
         self.games = games
-        self.tokenizer = tokenizer
+        self.token_dict = token_dict # This will just be a dictionary mapping from chess move to integer(Long dtype)
         self.max_length = max_length
 
     def __len__(self):
+        # print("inside len")
+        # print(f"len: {len(self.games)}")
         return len(self.games)
 
     def __getitem__(self, idx):
+        # print("inside getitem")
         # Remember, this is applied to one element of the batch, it doesn't see the other batch elements here
-        game = self.games[idx]
-        # Somewhere in between here have to apply the utility functions
-        tokenized_game = self.tokenizer(game, padding='max_length', truncation=True, max_length=self.max_length + 1, return_tensors="pt") # This is a buggy fix for the later use of the produce_pairs
+        game = self.games[idx].copy() # Have to make sure don't mutate it with each access, so just make a copy
 
-        # print(tokenized_game)
+        # Now, need to append the EOS token to the end of the tokenized game: if len() < max_length + 1, then add until it is max_length + 1
+        if len(game) < self.max_length + 1:
+            while len(game) < self.max_length + 1:
+                game.append('EOS')
+        elif len(game) > self.max_length + 1:
+            game = game[:self.max_length+1]
+        # Somewhere in between here have to apply the utility functions
+        tokenized_game = self.tokenize(game, self.token_dict)
+
+        # print(f"Tokenized game: {tokenized_game}")
+        # print(f"shape of tokenized game: {tokenized_game.shape}")
         
-        input_ids = tokenized_game['input_ids'].squeeze(0) 
-        loss_mask = tokenized_game['attention_mask'].squeeze(0) # This makes it so that the padded tokens have no effect on the loss
-        loss_mask = loss_mask[:-1] # This is to correct for the fact that the 
-        return input_ids, loss_mask # attention mask not only helps for attention but also will be useful for calculating loss to ignore padded tokens
+        EOS_token = len(self.token_dict) - 1
+        loss_mask = [0 if token == EOS_token else 1 for token in tokenized_game]
+        # input_ids = tokenized_game['input_ids'].squeeze(0) 
+        # loss_mask = tokenized_game['attention_mask'].squeeze(0) # This makes it so that the padded tokens have no effect on the loss
+        loss_mask = torch.tensor(loss_mask[:-1]) # This is to correct for the fact that the 
+        return tokenized_game, loss_mask # attention mask (encoder-only since we pad on the right anyway) not only helps for attention but also will be useful for calculating loss to ignore padded tokens
+    
+    def tokenize(self, game, token_dict):
+        # print("inside tokenize")
+        # game is a list of chess moves
+        for i, move in enumerate(game):
+            if move not in token_dict:
+                print(f"Move '{move}' not found in token_dict!")
+            game[i] = token_dict[move]
+            # print("error is in return statement")
+        return torch.tensor(game)
+                        
     
 if __name__ == '__main__':
-    f = open("/Users/hairanliang/Documents/Chess.com/MyGames2012-02_fixed.pgn")
+
+    file = "/Users/hairanliang/Documents/Chess.com/MyGames2012-02_fixed.pgn"
+    f = open(file)
 
     game_list = []
-
-    while True:
+    counter = 0
+    token_dict = {} # This will act as the tokenizer. 
+    game_counter = 0
+    while True:   # In the future, can think about modularizing this somehow
         game = chess.pgn.read_game(f)
         if game is None:
             break  # end of file
         
-        moves = game.board().variation_san(game.mainline_moves())
-        game_list.append(moves)
+        moves_list = []
+        # board = chess.Board()
+        board = game.board()  
+        for move in game.mainline_moves():
+            san_move = board.san(move)
+            # print(san_move)
+            moves_list.append(san_move)
+
+            if san_move not in token_dict:
+                token_dict[san_move] = counter
+                counter += 1
+            
+            board.push(move)
+        
+        game_list.append(moves_list) # This should be a list of lists, where each list is one chess game in standard notation (SAN), each move is one element in the list, no more numbers
+        game_counter += 1
+
+    token_dict['EOS'] = len(token_dict) # Adding in an EOS/padding token
+
+    vocab_size = len(token_dict)
+    print(f"number of games: {game_counter}")
+    print(f"tokenizer dict: {token_dict}")
+    print(f"vocab_size: {vocab_size}")
+    print(f"game list: {game_list}")
+
+
+    ds = ChessDataset(game_list, token_dict, max_length=120)
+
+    print(f"length of ds inside main: {len(ds)}")
+    print(f"first ds element inside main{ds[0]}")
+    print(ds[0])
+    # for u in range(len(ds)):
+    #     print(u)
+    #     print(ds[u])
+    # f = open("/Users/hairanliang/Documents/Chess.com/MyGames2012-02_fixed.pgn")
+
+    # game_list = []
+
+    # while True:
+    #     game = chess.pgn.read_game(f)
+    #     if game is None:
+    #         break  # end of file
+        
+    #     moves = game.board().variation_san(game.mainline_moves())
+    #     game_list.append(moves)
     
-    game_lengths = [len(game) for game in game_list]
+    # game_lengths = [len(game) for game in game_list]
 
-    # plt.hist(game_lengths, bins=50)
-    # plt.xlabel('Game Length')
-    # plt.ylabel('Frequency')
-    # plt.title('Distribution of Game Lengths')
-    # plt.show()
+    # # plt.hist(game_lengths, bins=50)
+    # # plt.xlabel('Game Length')
+    # # plt.ylabel('Frequency')
+    # # plt.title('Distribution of Game Lengths')
+    # # plt.show()
 
-    max_length = int(np.percentile(game_lengths, 95))
-    print(f"Chosen max_length: {max_length}")
-    ds = ChessDataset(game_list, tokenizer, 10)
+    # max_length = int(np.percentile(game_lengths, 95))
+    # print(f"Chosen max_length: {max_length}")
+    # ds = ChessDataset(game_list, tokenizer, 10)
 
-    # Checking all the dataset values to check for something fishy
+    # # Checking all the dataset values to check for something fishy
 
-    print(len(ds))
-    for u in range(len(ds)):
-        print(ds[u])
+    # print(len(ds))
+    # for u in range(len(ds)):
+    #     print(ds[u])
 
     # Aha, I've found some games that actually never started...
     
--- train.py
+++ train.py
@@ -12,29 +12,65 @@ import itertools
 from util import produce_pairs
 
 # Initialize tokenizer
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-tokenizer.pad_token = tokenizer.eos_token
+# tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+# tokenizer.pad_token = tokenizer.eos_token
 
 # Load and preprocess the data
-file = "/Users/hairanliang/Documents/Chess.com/MyGames2012-02_fixed.pgn"
-f = open(file)
-game_list = []
+# file = "/Users/hairanliang/Documents/Chess.com/MyGames2012-02_fixed.pgn"
+# f = open(file)
+# game_list = []
 
-while True:
+# while True:
+#     game = chess.pgn.read_game(f)
+#     if game is None:
+#         break  # end of file
+    
+#     moves = game.board().variation_san(game.mainline_moves())
+#     game_list.append(moves)
+
+# lissy = tokenizer(game_list)['input_ids']
+# lissy = list(itertools.chain.from_iterable(lissy))
+# vocab_size = 50257
+
+file = "/Users/hairanliang/Documents/Chess.com/all_games_hairan.pgn"
+f = open(file)
+
+game_list = []
+counter = 0
+token_dict = {} # This will act as the tokenizer. 
+game_counter = 0
+while True:   # In the future, can think about modularizing this somehow
     game = chess.pgn.read_game(f)
     if game is None:
         break  # end of file
     
-    moves = game.board().variation_san(game.mainline_moves())
-    game_list.append(moves)
+    moves_list = []
+    # board = chess.Board()
+    board = game.board()  
+    for move in game.mainline_moves():
+        san_move = board.san(move)
+        # print(san_move)
+        moves_list.append(san_move)
 
-lissy = tokenizer(game_list)['input_ids']
-lissy = list(itertools.chain.from_iterable(lissy))
-vocab_size = 50257
+        if san_move not in token_dict:
+            token_dict[san_move] = counter
+            counter += 1
+        
+        board.push(move)
+    
+    game_list.append(moves_list) # This should be a list of lists, where each list is one chess game in standard notation (SAN), each move is one element in the list, no more numbers
+    game_counter += 1
+
+token_dict['EOS'] = len(token_dict) # Adding in an EOS/padding token
+
+vocab_size = len(token_dict)
+print(f"number of games: {game_counter}")
+print(f"tokenizer dict: {token_dict}")
+print(f"vocab_size: {vocab_size}")
 
 # Initialize model, dataset, and dataloader
-model = Decoder(vocab_size=vocab_size, block_size=100, num_dec_blocks=1, emb_dim=256, model_dim=256, num_heads=2)
-ds = ChessDataset(game_list, tokenizer, max_length=100)
+model = Decoder(vocab_size=vocab_size, block_size=120, num_dec_blocks=6, emb_dim=512, model_dim=512, num_heads=2)
+ds = ChessDataset(game_list, token_dict, max_length=120)
 batch_size = 25
 dataloader = DataLoader(ds, batch_size=batch_size, shuffle=True)
 
@@ -66,3 +102,8 @@ for epoch in range(num_epochs):
         optimizer.step()
 
         print(f"Epoch {epoch}, Batch {i}, Loss: {loss.item()}")
+
+
+# Next step is to scale up. Getting loss down from 8.05 to 8.00, but likely need WAY more games. Right now only have 100 LOL. Time for the lichess dataset oh god...
+
+# Might want to build a generate function as well.
